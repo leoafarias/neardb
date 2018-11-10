61d73cd3d3102a9695c8bf2865be6f9b397171be
Leo Farias <leo@fanhero.com>
2018-11-10 14:46:12 -0500

    Added more tests for utils and collection, doc changes

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,6 +1,14 @@
-import { documentPath } from '../src/utils'
+import { documentPath, reservedKey, uuid } from '../src/utils'
 
 jest.setTimeout(5000)
+
+function isGuid(uuid: string) {
+  if (uuid[0] === '{') {
+    uuid = uuid.substring(1, uuid.length - 1)
+  }
+  const regexGuid = /^(\{){0,1}[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}(\}){0,1}$/gi
+  return regexGuid.test(uuid)
+}
 
 describe('documentPath', () => {
   const docPath = [
@@ -37,5 +45,30 @@ describe('documentPath', () => {
     expect('collection').toBe(colPath[colPath.length - 1].type)
     expect(item[0]).toBe(colPath[colPath.length - 1].key)
     expect(item[1]).toBeFalsy()
+  })
+})
+
+describe('reservedKey', () => {
+  it('the following keys are served', () => {
+    const reservedWords = ['_meta', 'collection', 'doc', 'indices']
+    expect.assertions(reservedWords.length)
+    reservedWords.forEach(item => {
+      if (reservedKey(item)) {
+        expect(true).toBe(true)
+      } else {
+        throw new Error(item + ': should be a reserved word')
+      }
+    })
+  })
+})
+
+describe('uuid', () => {
+  it('valid uuid', () => {
+    let uuidValue = uuid()
+    if (isGuid(uuidValue)) {
+      expect(uuidValue).toBeTruthy()
+    } else {
+      throw new Error('Not a valid UUID')
+    }
   })
 })
